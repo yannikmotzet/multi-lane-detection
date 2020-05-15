abc3d4ad82677d42af03e98c2f6f7e806922cca3
ymotzet <yannik.motzet@outlook.com>
2020-05-15 23:44:25 +0200

    implemented upd communication to steering in laneassist

--- scripts/laneassist_dummy.py
+++ scripts/laneassist_dummy.py
@@ -5,6 +5,7 @@ import numpy as np
 import cv2
 import random
 import math
+import socket
 
 __author__ = 'author'
 __copyright__ = 'Copyright 2020, ZF DHBW Innovation Lab'
@@ -12,12 +13,29 @@ __version__ = '0.1.0'
 __email__ = 'author@email.com'
 __status__ = 'in development'
 
+UDP_IP = "192.168.0.90"
+UDP_PORT = 10002
+
+
 
 def callback(data):
     rospy.loginfo("offset: " + str(int(data.data)))
 
+    offset = data.data
+    
+    # p regulator
+    steering_angle = 0.15 * offset
     # TODO PID -> steering angle
-    # TODO send angle to TruckMaker/Truck steering
+
+    # build content for UDP message
+    message = "STEER,1," + str(steering_angle)
+    message_udp = str.encode(message)
+
+    # create socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    # send UDP message
+    sock.sendto(message_udp, (UDP_IP, UDP_PORT))
     
     
 def listener():
